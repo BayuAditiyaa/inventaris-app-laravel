a15e764b85515b594bbdd964fe302d75247e97db
baybrz <shinatsuka7@gmail.com>
2026-03-29 00:25:00 +0700

    adding policies for user and product

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Product;
+use App\Models\User;
+
+class ProductPolicy
+{
+
+//anyone authenticated can view
+public function viewAny(User $user): bool
+{
+    return true;
+}
+
+public function view(User $user, Product $product): bool
+{
+    return true;
+}
+
+// only admin can create
+public function create(User $user): bool
+{
+    return $user->role == 'admin';
+}
+
+//only admin can update
+public function update(User $user, Product $product): bool
+{
+    return $user->role == 'admin';
+}
+
+//only admin can delete
+public function delete(User $user, Product $product): bool
+{
+    return $user->role == 'admin';
+}
+
+
+}
